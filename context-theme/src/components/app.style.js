142a30f44035fd67cdf761a44a1007515fc42480
guilherme <85708476+guic170602@users.noreply.github.com>
2022-06-07 18:59:38 -0300

    Final dark light

---
+++ context-theme/src/components/app.style.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+//Estilização do componente AppContainer
+export const AppContainer = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  box-sizing: border-box;
+  padding: 300px;
+
+// Abaixo passamos para as propriedades background-color e color
+// as configurações de estilo definidas no arquivo
+// theme.js e compartilhada pela props theme do componente <ThemeProvider>
+
+  background-color: ${(props) => props.theme.backgroundColor};
+  color: ${(props) => props.theme.color};
+  transition: background-color 0.8s linear, color 0.5s linear;
+`;
+
+//Estilização do componente MainSection
+export const MainSection = styled.div`
+text-align: center;
+`;
